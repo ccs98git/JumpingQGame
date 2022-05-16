991676c67b5962c375f3db9d2f05ab99cbac9c22
ccs98git <105653637+ccs98git@users.noreply.github.com>
2022-05-16 16:16:29 +0200

    Add files via upload

---
+++ Assets/BoyPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoyPlayer : MonoBehaviour
+{
+    Rigidbody2D boyRB;
+    public float maxVel;
+    bool turnBoy = true;
+    SpriteRenderer boyRender;
+    Animator boyAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boyRB = GetComponent<Rigidbody2D>();
+        boyRender = GetComponent<SpriteRenderer>();
+        boyAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        if(move>0 && !turnBoy){
+            turn();
+        }else if(move<0 && turnBoy){
+            turn();
+        }
+
+        boyRB.velocity = new Vector2(move*maxVel, boyRB.velocity.y);
+
+        //running
+        boyAnim.SetFloat("VelMovement",Mathf.Abs(move));
+    }
+
+    void turn(){
+        turnBoy = !turnBoy;
+        boyRender.flipX = !boyRender.flipX;
+    }
+
+
+}
